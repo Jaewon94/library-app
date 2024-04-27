3f621685699e302f8095be9bc1a67dd756ba852d
Jaewon94 <wodnjs3580@gmail.com>
2024-04-27 20:36:01 +0900

    섹션1

---
+++ src/main/java/com/group/libraryapp/dto/user/response/user/UserResponse.java
@@ -0,0 +1,28 @@
+package com.group.libraryapp.dto.user.response.user;
+
+import com.group.libraryapp.domain.user.User;
+
+public class UserResponse {
+
+    private Long id;
+    private String name;
+    private Integer age;
+
+    public UserResponse(Long id, User user) {
+        this.id = id;
+        this.name = user.getName();
+        this.age = user.getAge();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+}
