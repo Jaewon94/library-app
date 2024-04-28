c10fbb958154c988506721c0f220e30ec74f0422
Jaewon94 <wodnjs3580@gmail.com>
2024-04-28 11:46:06 +0900

    섹션3

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -1,71 +1,42 @@
 package com.group.libraryapp.controller.user;
 
-import com.group.libraryapp.domain.user.User;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.user.UserResponse;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
+import com.group.libraryapp.service.user.UserService;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class UserController {
 
-    private final JdbcTemplate jdbcTemplate;
+    private final UserService userService;
 
-    public UserController(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @PostMapping("/user")
     public void saveUser(@RequestBody UserCreateRequest request) {
-        String sql = "INSERT INTO user (name, age) VALUES (?, ?)";
-        jdbcTemplate.update(sql, request.getName(), request.getAge());
+        userService.saveUser(request);
     }
 
     @GetMapping("/user")
     public List<UserResponse> getUsers() {
-
-       String sql = "select * from user";
-       return jdbcTemplate.query(sql, (rs, rowNum) -> {
-           long id = rs.getLong("id");
-           String name = rs.getString("name");
-           int age = rs.getInt("age");
-           return new UserResponse(id, name, age);
-       });
+        return userService.getUsers();
     }
 
     @PutMapping("/user")
     public void updateUser(@RequestBody UserUpdateRequest request){
+        userService.updateUser(request);
 
-        String readSql = "select * from user where id = ?";
-        boolean isUserNotExist = jdbcTemplate.query(readSql, (rs, rowNum) -> 0, request.getId()).isEmpty();
-
-        if(isUserNotExist){
-            throw new IllegalArgumentException("User not found");
-        }
-
-        String sql = "update user set name=? where id=?";
-        jdbcTemplate.update(sql, request.getName(), request.getId());
     }
 
     @DeleteMapping("/user")
     public void deleteUser(@RequestParam String name) {
+        userService.deleteUser(name);
 
-        String readSql = "select * from user where name = ?";
-        boolean isUserNotExist = jdbcTemplate.query(readSql, (rs, rowNum) -> 0, name).isEmpty();
-
-        if(isUserNotExist){
-            throw new IllegalArgumentException("User not found");
-        }
-
-        String sql = "delete from user where name=?";
-        jdbcTemplate.update(sql, name);
     }
 
 }
